44af0bf24bb01f3e646ee3334c5c4d65b5903fe6
Sumit2629 <156787318+Sumit2629@users.noreply.github.com>
2025-07-17 18:32:07 +0000

    3

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,67 @@
+// let score="33"
+// let sco="33abc"
+// let sc=null
+// let s=undefined
+// let boo=true
+
+// console.log(score)
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumber=Number(score)
+
+// let valueInNumbe=Number(sco)
+
+// let valueInNull=Number(sc)
+
+// let valueInUndef=Number(s)
+
+// let valueInbool=Number(boo)
+
+// console.log(typeof valueInNumber)
+
+// console.log(typeof valueInNumbe)
+
+// console.log(valueInNumbe) // it results NaN which means not a number
+
+// console.log(typeof valueInNull)
+
+// console.log(valueInNull)
+
+// console.log(typeof s)
+
+// console.log(s)
+
+// console.log(typeof valueInbool)
+
+// console.log(boo)
+
+
+
+// "33"=> 33
+// "33abc"=> NaN
+// true=>1; false=>0
+
+
+// conversion to boolean
+
+// let isLoggedIn=1 // means true
+// let isLogged=""  // means false
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// let booleanIsLogged=Boolean(isLogged)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+// console.log(typeof booleanIsLogged)
+// console.log(booleanIsLogged)
+
+// 1=> true 
+// 0=> false
+// ""=> false
+// "sumit"=> true
+
+
+// conversion to string
+let num=33
+let StringNum=String(num)
+console.log(typeof StringNum)
+console.log(StringNum)
