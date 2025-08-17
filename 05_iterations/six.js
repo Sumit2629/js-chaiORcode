54283a74d3db67f61f1f198276dd3e963f0ea419
Sumit2629 <156787318+Sumit2629@users.noreply.github.com>
2025-08-17 17:42:00 +0000

    17-08

---
+++ 05_iterations/six.js
@@ -0,0 +1,20 @@
+// const coding=["java","python","cpp","js"]
+// const values=coding.forEach((i)=>{
+//     console.log(i)
+//     return i
+// })
+// console.log(values)  // this state it doesnot return any value
+
+
+
+const nums=[1,2,3,4,5,6,7,8,9,10]
+// const newNums=nums.filter((i) => i>4) // this is normal 
+
+// const newNums=nums.filter((i) => {
+//     return i>4 // we have to use return if we dont it return empty array
+// }) 
+
+
+
+
+console.log(newNums)
